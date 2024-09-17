a96d427a91db47d0bf529907bc849439ed7b13f6
benz <benzvanbouwel12@gmail.com>
2024-09-17 16:35:50 +0200

    fixed back to without clouds

--- T-rex.py
+++ T-rex.py
@@ -18,9 +18,9 @@ large_cactus = [pygame.image.load("Assets\Cactus\LargeCactus1.png"),pygame.image
 
 bird = [pygame.image.load("Assets\Bird\Bird1.png"),pygame.image.load("Assets\Bird\Bird2.png")]
 
-cloud = [pygame.image.load("Assets\background\Cloud.png")]
+cloud = [pygame.image.load("Assets\other\Cloud.png")]
 
-bg = [pygame.image.load("Assets\background\Track.png")]
+bg = [pygame.image.load("Assets\other\Track.png")]
 
 class Dinosaur:
     x_pos = 80
@@ -96,21 +96,6 @@ class Dinosaur:
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
 
 
-class Cloud:
-    def __init__(self):
-        self.x = screen_width + random.randint(800, 1000)
-        self.y = random.randint(50, 100)
-        self.image = cloud
-        self.width = self.image.get_width()
-        
-    def update(self):
-        self.x -= game_speed // 2
-        if self.x < -self.width:
-            self.x = screen_width + random.randint(2500, 3000)
-            self.y = random.randint(50, 100)
-        
-    def draw(self, screen):
-        screen.blit(self.image, (self.x, self.y))
         
 
 
@@ -118,8 +103,7 @@ def main():
     run = True
     clock = pygame.time.Clock()
     player = Dinosaur()
-    clouds = Cloud()
-    game_speed = 14
+
     
     while run:
         for event in pygame.event.get():
@@ -132,8 +116,6 @@ def main():
         player.draw(screen)
         player.update(userinput)
         
-        cloud.draw(screen)
-        cloud.update()
         
         clock.tick(30)
         pygame.display.update()
